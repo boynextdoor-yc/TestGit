0fc35ac13de95ef2d7e4ff77c615b363dee45186
yc_glo <yc_glo@qq.com>
2019-12-20 14:46:20 +0800

    first commit

---
+++ src/test/java/com/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
